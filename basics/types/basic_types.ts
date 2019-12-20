2b5f1eb0e1998590d9296725728261a8f83d8292
Veerabhadrudu <veerabhadrudu.s@gmail.com>
2019-12-20 10:40:31 +0530

    Typescript Programs

---
+++ basics/types/basic_types.ts
@@ -0,0 +1,10 @@
+// Number type.
+let count: number = 1;
+// boolean type.
+let isDone: boolean = true;
+// string type.
+let myName: string = "Veerabhadrudu";
+
+console.log(`Present count is ${count}`);
+console.log(`Is Done ${isDone}`);
+console.log(`My Name is ${myName}`);
