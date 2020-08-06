a01a26d1712d6a793c67486a2e6a8fad74fbb1e0
Veerabhadrudu <veerabhadrudu.s@hpe.com>
2020-08-06 11:34:20 +0530

    Type script examples

--- basics/types/union_types.ts
+++ basics/types/union_types.ts
@@ -1,9 +1,9 @@
-let PERSON: string = "PERSON";
+let BOAT: string = "BOAT";
 let VEHICLE: string = "VEHICLE";
 
-class Person {
-    type: string = PERSON;
-    constructor(public payload: { name: string, age: number }) { }
+class Boat {
+    type: string = BOAT;
+    constructor(public payload: { name: string, locationId: number }) { }
 }
 
 class Vehicle {
@@ -12,16 +12,16 @@ class Vehicle {
 }
 
 
-type UnionType = Person | Vehicle;
+type UnionType = Boat | Vehicle;
 
 function printUnionType(unionTypeInstance: UnionType) {
-    console.log(`Is unionTypeInstance is instance of Person ${unionTypeInstance instanceof Person}`);
-    console.log(`Is unionTypeInstance is instance of vehicle ${unionTypeInstance instanceof Vehicle}`);
+    console.log(`Is unionTypeInstance is instance of Boat ${unionTypeInstance instanceof Boat}`);
+    console.log(`Is unionTypeInstance is instance of Vehicle ${unionTypeInstance instanceof Vehicle}`);
     console.log(`Payload value is `, unionTypeInstance.payload);
     switch (unionTypeInstance.type) {
-        case PERSON:
-            let personPayload: { name: string, age: number } = (unionTypeInstance.payload as { name: string, age: number });
-            console.log(personPayload.age);
+        case BOAT:
+            let boatPayload: { name: string, locationId: number } = (unionTypeInstance.payload as { name: string, locationId: number });
+            console.log(boatPayload.locationId);
             break;
         case VEHICLE:
             let vehiclePayload: { type: string, id: string } = (unionTypeInstance.payload as { type: string, id: string });
@@ -32,7 +32,7 @@ function printUnionType(unionTypeInstance: UnionType) {
     }
 }
 
-let unionTypeInstance1: UnionType = new Person({ name: 'Veera', age: 32 });
+let unionTypeInstance1: UnionType = new Boat({ name: 'Veera', locationId: 32 });
 let unionTypeInstance2: UnionType = new Vehicle({ type: 'Bike', id: 'KA53ER8044' });
 
 printUnionType(unionTypeInstance1);
